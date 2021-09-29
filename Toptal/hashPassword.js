d8b8b44362a45bb3cf6cf4c101a328cd5e9820f2
Tolibjon <tolibjohn@gmail.com>
2021-09-29 12:44:10 +0500

    hashPassword challenge solution

--- Toptal/hashPassword.js
+++ Toptal/hashPassword.js
@@ -2,5 +2,43 @@ function hashPassword(password, x) {
   // password is a string, x is a number
   // return a string
   // (ex. password = 'ab1By', x = 3 so you should return "DE4eB")
+    splitted = password.split``.map(char=>changeLetters(char, x))
 
+    function changeLetters(letter, step){
+      let code=letter.charCodeAt();
+      if(code>96 && code<123){
+        code=letter.toUpperCase().charCodeAt()
+      }
+      else if(code>64 && code<91){
+        code=letter.toLowerCase().charCodeAt()
+      }
+
+
+      if(code>96 && code<123){
+        let pos = code+step;
+        if(pos>122){
+          pos=96+(pos-122)
+        }
+        return String.fromCharCode(pos)
+      }
+      else if(code>64 && code<91){
+        let pos = code+step;
+        if(pos>90){
+          pos=64+(pos-90)
+        }
+        return String.fromCharCode(pos)
+      }
+      else if(code>47 && code<58){
+        let pos = letter.charCodeAt()+step;
+        if(pos>57){
+          pos=47+(pos-57)
+        }
+        return String.fromCharCode(pos)
+      }
+      
+    }
+
+    return splitted.join``
 };
+
+console.log(hashPassword('ab1By', 3));
