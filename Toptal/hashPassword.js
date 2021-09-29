46fc376ae8c62d08009a6d1165a9af142266c13f
Tolibjon <tolibjohn@gmail.com>
2021-09-29 09:38:49 +0500

    Create twoArrayAvg challenge

---
+++ Toptal/hashPassword.js
@@ -0,0 +1,8 @@
+function hashPassword(password, x) {
+  // password is a string, x is a number
+  // return a string
+  // (ex. password = 'ab1By', x = 3 so you should return "DE4eB")
+
+};
+
+console.log(hashPassword('09', 1));
