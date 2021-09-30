a629f24342955cd4a4a4343b9ad40b07ac2bffef
Tolibjon Tolibov <tolibjohn@gmail.com>
2021-09-30 12:27:08 +0500

    Update averageAsciiChar.js

--- Toptal/averageAsciiChar.js
+++ Toptal/averageAsciiChar.js
@@ -1,2 +1,12 @@
-let avg=x.split('').map(l=>l.charCodeAt()).reduce((a,b)=>a+b)/x.length
-return String.fromCharCode(Math.round(avg))
+// box.averageAsciiChar = function averageAsciiChar(x) {
+//   // x is a string
+//   // return a character
+//   // (ex. x="Hello World!", you should return "Z")
+  
+// };
+
+// SOLUTION:
+function averageAsciiChar(x) {
+  let avg=x.split('').map(l=>l.charCodeAt()).reduce((a,b)=>a+b)/x.length
+  return String.fromCharCode(Math.round(avg))
+};
