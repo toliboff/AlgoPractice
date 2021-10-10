6473da148c158b90e782d81e17798d8d4740c037
Tolibjon <tolibjohn@gmail.com>
2021-10-10 19:04:49 +0500

    Create challenge binaryToNumber

---
+++ Toptal/binaryToNumber.js
@@ -0,0 +1,8 @@
+function binaryToNumber(x) {
+  // x is a binary number.
+  // return a number
+  // (ex. x=1001, you should return 9)
+  return parseInt(x, 2)
+};
+
+console.log(binaryToNumber(1001));
