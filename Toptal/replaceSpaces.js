af9055f618abdbd0d8acfbcef534753dfdc2a96c
Tolibjon <tolibjohn@gmail.com>
2021-10-10 19:07:05 +0500

    Create challenge replaceSpaces

---
+++ Toptal/replaceSpaces.js
@@ -0,0 +1,9 @@
+// box.replaceSpaces = 
+function replaceSpaces(x) {
+  // x is a string
+  // return a string
+  // (ex. x="hello world", you should return "hello%20world")
+  return x.replace(/\s/g, '%')
+};
+
+console.log(replaceSpaces("hello world"));
